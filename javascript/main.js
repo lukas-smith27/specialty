061e74325caf3a5fab7159e10589f3f8ee3d772d
Lukas Smith <lmssmith2000@gmail.com>
2023-03-13 13:41:59 -0400

    Remove public folder for hosting

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".small-menu").click(function(){
+        if (!$(".links").hasClass("active")) {
+            $(".links").addClass("active")
+        } else {
+            $(".links").removeClass("active")
+            window.location.scrollTop()
+        }
+    }) 
+})
+
